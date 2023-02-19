87076730084a8008930b9dcac898daae6a816087
bluepill5 <alexandro.mayoral@gmail.com>
2023-02-18 21:26:19 -0600

    Modifiying canvas

---
+++ game.js
@@ -0,0 +1,15 @@
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+window.addEventListener('load', startGame);
+
+function startGame() {
+    window.innerHeight
+    window.innerWidth
+    // game.fillRect(0, 0, 100, 100);
+    // game.clearRect(0, 0, 50, 100);
+}
+
+
+
+

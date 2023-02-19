96100c5cbb5e3be36f491829a1f5a23bd4921f0a
bluepill5 <alexandro.mayoral@gmail.com>
2023-02-19 16:19:06 -0600

    Defining canvas

--- game.js
+++ game.js
@@ -1,13 +1,35 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame);
+let canvasSize;
+let elementSize;
+
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
 
 function startGame() {
-    window.innerHeight
-    window.innerWidth
-    // game.fillRect(0, 0, 100, 100);
-    // game.clearRect(0, 0, 50, 100);
+    game.font = elementSize + 'px Verdana';
+    game.textAlign = 'end';
+    
+    for(let i = 1; i <= 10; i++) {
+        game.fillText(emojis['X'], elementSize * i, elementSize);
+    }
+}
+
+function setCanvasSize() {
+
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8;
+    } else {
+        canvasSize = window.innerHeight * 0.8;
+    }
+    
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+
+    elementSize = (canvasSize / 10);
+
+    startGame();
 }
 
 
